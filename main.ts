4ea6320e8fe1aaddd9876101af2448fe993ec38d
Tim1D24 <124782633+Tim1D24@users.noreply.github.com>
2023-03-20 12:02:24 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,14 @@
+let a = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    a = 0
+    basic.showNumber(a)
+})
+input.onButtonPressed(Button.B, function () {
+    a += 1
+})
+basic.forever(function () {
+	
+})
